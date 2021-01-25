f493a48f004e3143e5866b6ff9614a897d516d0e
Pedro Miguel <pedromrap@gmail.com>
2021-01-25 15:21:27 -0300

    initial commit

---
+++ aluraquiz/pages/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.secondary};
+`
+
+export default function Home() {
+  return (
+    <Title>Hello World</Title>
+  )
+}
